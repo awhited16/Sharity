96095b48352d8b628cfab63159444442fba25f96
awhited16 <52683023+awhited16@users.noreply.github.com>
2020-01-07 19:54:22 -0500

    set up file structure for react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Discover from "./pages/Discover";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+  return (
+
+  );
+}
+
+export default App;
